1a6d3f1c9308d081ed8bc5101503cfb3189725a8
Kallyan Singha <singhakallyan@gmail.com>
2024-02-06 16:54:11 +0530

    style: design improvements

--- public/verifyOtpForm.php
+++ public/verifyOtpForm.php
@@ -1,15 +1,27 @@
-<?php require "verifyOtp.php" ;
+<?php require "verifyOtp.php" ;?>
 
-echo "OTP verification";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="/view/css/global.css">
+    <link rel="stylesheet" href="/view/css/otpverification.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
 
-<form method="POST" action="verifyOtp.php">
-<label for="action">Action</label>
-        <input type="text" name="action" id="" value="<?php if(!empty($_GET['action'])) echo $_GET["action"]; ?>">
-        <label for="email">Email Id</label>
-        <input type="text" name="email" id="" value="<?php if(!empty($_GET['email'])) echo $_GET["email"]; ?>">
-        <label for="otp">OTP</label>
-        <input type="number" name="otp" id="">
-        <input type="submit" value="Submit" name="submit">
+
+<form method="POST" action="verifyOtp.php" class="otp_form">
+
+        <input type="text" name="action" id="otp_action" value="<?php if(!empty($_GET['action'])) echo $_GET["action"]; ?>">
+        
+        <input type="text" name="email" id="otp_email" value="<?php if(!empty($_GET['email'])) echo $_GET["email"]; ?>">
+        
+        <input type="number" name="otp" id="otp_value" required placeholder="Enter Your Otp">
+        <input type="submit" value="Submit" name="submit" class="otp_submit">
     </form>
+
+    </body>
+</html>
